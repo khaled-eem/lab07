17350bcfd98644c8693d85f5e81b9de9d3843748
khaled-eem <khaled.3.mm@hotmail.com>
2024-04-20 03:40:29 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function Change() {
+    let colorInput = document.getElementById("color").value;
+    let header = document.getElementById("header");
+    header.style.color = colorInput;
+    console.log(colorInput)
+}
